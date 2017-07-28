8f3e7e7df5f268716c292975c1cc8e03b440d473
Isabella-Bowland <30536840+Isabella-Bowland@users.noreply.github.com>
2017-07-28 15:38:56 -0600

    Add files via upload

---
+++ madlibs.js
@@ -0,0 +1,15 @@
+
+
+
+function save() {
+  debugger
+  var word1 = document.getElementById('w1').value;
+  var word2 = document.getElementById('w2').value;
+  var word3 = document.getElementById('w3').value;
+  var word4 = document.getElementById('w4').value;
+  var word5 = document.getElementById('w5').value;
+}
+
+function display () {
+  document.write("Once upon a time" + word1);
+}
